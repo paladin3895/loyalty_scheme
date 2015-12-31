0ffb81a2ec5e2845037f17c774e9f27535a58046
Pham Do Duy Dat <datpdd@cpu-datpdd.(none)>
2015-12-31 14:03:39 +0700

    improved response of component controller

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -5,16 +5,23 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\BaseApiController;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Formatters\ArrayFormatter;
 
 class ComponentController extends BaseApiController
 {
     public function getPolicies()
     {
-        return (\Liquid\Builders\PolicyBuilder::getPolicyFormats());
+        return $this->response->array([
+            'status' => 1,
+            'data' => \Liquid\Builders\PolicyBuilder::getPolicyFormats()
+        ]);
     }
 
     public function getRewards()
     {
-        return (\Liquid\Builders\PolicyBuilder::getRewardFormats());
+        return $this->response->array([
+            'status' => 1,
+            'data' => \Liquid\Builders\PolicyBuilder::getRewardFormats()
+        ]);
     }
 }
